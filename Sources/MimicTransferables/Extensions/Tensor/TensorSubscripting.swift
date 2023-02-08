9b3cfe3ff2e34b05b7dc78b01dbee63b79c0fcf8
briankthsmth <110857165+briankthsmth@users.noreply.github.com>
2023-02-08 09:31:02 -0700

    Added missing copyright notices and some extra documentation.

--- Sources/MimicTransferables/Extensions/Tensor/TensorSubscripting.swift
+++ Sources/MimicTransferables/Extensions/Tensor/TensorSubscripting.swift
@@ -1,6 +1,18 @@
 //
-//  File.swift
-//  
+//  Copyright 2023 Brian Keith Smith
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 //
 //  Created by Brian Smith on 1/25/23.
 //
@@ -11,9 +23,9 @@ extension Tensor {
     /// Subscripting method for tensors.
     ///
     ///  - Parameters:
-    ///  - range: The range of the sub-tensor to create.
+    ///    - range: The range of the sub-tensor to create.
     ///
-    ///  - Returns: A sub-tensor for the given range.
+    ///  - Returns: A new Tensor for the given range.
     public subscript(range: Range<Index>) -> Tensor {
         let subshape = shape[1...]
         let subshapeSize = subshape.isEmpty ? 1 : subshape.reduce(1, *)
@@ -26,7 +38,12 @@ extension Tensor {
                dataType: dataType,
                featureChannelPosition: featureChannelPosition)
     }
-
+    /// Subscripting method using a closed range.
+    ///
+    /// - Parameters:
+    ///   - range: A closed range to extract from the tensor.
+    ///
+    /// - Returns: A new Tensor for the closed range.
     public subscript(range: ClosedRange<Index>) -> Tensor {
         self[Range(range)]
     }
